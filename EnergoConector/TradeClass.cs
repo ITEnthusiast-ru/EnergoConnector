95429c732414dad221eba31fc73c9c0f7556fae2
Igor Bazanov <site-create-x@yandex.ru>
2025-02-02 10:46:50 +0700

    Add several classes

--- EnergoConector/TradeClass.cs
+++ EnergoConector/TradeClass.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using Gurux.DLMS;
+using Gurux.DLMS.Objects;
+using Gurux.DLMS.Plc;
+using Gurux.DLMS.Enums;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Ports;
+using InTheHand.Net.Sockets;
 
 namespace EnergoConector
 {
     internal class TradeClass
     {
+        string btDeviceAddress = "XX:XX:XX:XX:XX:XX";  // Укажите MAC-адрес вашего Bluetooth-устройства
+        string btPortName = "COMx";  // Укажите порт Bluetooth, который используется (например, COM3)
+
+        public void SerachBLE() {
+            BluetoothDeviceInfo device = null;
+            foreach (BluetoothDeviceInfo dev in BluetoothDeviceInfo.)
+            {
+                if (dev.DeviceAddress.ToString() == btDeviceAddress)
+                {
+                    device = dev;
+                    break;
+                }
+                if (device == null)
+                {
+                    Console.WriteLine("Устройство не найдено.");
+                    return;
+                }
+
+            }
+        }
+
     }
+          
+    
+    
 }
